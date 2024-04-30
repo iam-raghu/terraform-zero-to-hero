0f807c75184c82e43c20eb8e3268ce65072cac13
Raghunath Kushawaha <78621381+iam-raghu@users.noreply.github.com>
2024-04-30 16:54:50 +0530

    Initial Files for Day-4
    Initial Files for Day-4

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "raghunath" {
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  ami = "ami-0f58b397bc5c1f2e8" # change this
+  #subnet_id = "subnet-019ea91ed9b5252e7" # change this
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+resource "aws_s3_bucket" "raghuraghuraghuraghu-s3" {
+  bucket = "raghuraghuraghuraghu-s3" # change this
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
