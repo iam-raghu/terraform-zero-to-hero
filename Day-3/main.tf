596a9ae9a4c94140e9fd9f5028a0e9e098680300
Raghunath Kushawaha <78621381+iamraghu149@users.noreply.github.com>
2024-04-30 11:46:49 +0530

    Initial files for Day-3

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -6,5 +6,5 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-0f58b397bc5c1f2e8" # replace this
   instance_type_value = "t2.micro"
-  #subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  #subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this.
 }
