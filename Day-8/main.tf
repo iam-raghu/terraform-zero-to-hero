48b9420c9dc96cdb353b072a4082567918ae2315
Raghunath Kushawaha <78621381+iam-raghu@users.noreply.github.com>
2024-05-05 12:12:14 +0530

    Added Files for Day-8

--- Day-8/main.tf
+++ Day-8/main.tf
@@ -11,3 +11,5 @@ import {
 
 # For this command we will get the complete configuration using it and then copy and paste the configuration in main.tf after removing the "import details". and as well as delete resources.tf files as well from VS Code You will see it how to use it inside Scenario folder in main.tf.
 # terraform plan -generate-config-out=generated_resoureces.tf
+# Below command is needed to run to importthe real-time server
+# terraform import aws_instance.migrated i-0c559bb39ed99c508
